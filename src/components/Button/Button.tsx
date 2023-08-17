32957349d7eccb683b70c8dc2b5f688e3d613491
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-17 12:56:00 -0300

    Added variants to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 interface Props {
   children: React.ReactNode;
+  variant?: 'primary' | 'secondary';
 }
 
-const Button = ({ children }: Props) => {
-  return <button>{children}</button>;
+const Button = ({ children, variant = 'primary' }: Props) => {
+  const variants = {
+    primary: 'btn-primary',
+    secondary: 'btn-secondary',
+  };
+
+  return (
+    <button
+      className={`${variants[variant]} px-8 border py-4 rounded text-headlines font-bold`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- test/components/Button/Button.test.tsx
+++ test/components/Button/Button.test.tsx
@@ -1,11 +1,32 @@
-import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import Button from '../../../src/components/Button/Button';
 
-test('Renders correctly the label', () => {
-  const button = render(<Button>{buttonLabel}</Button>);
+describe('children prop', () => {
+  test('Renders correctly the label', () => {
+    const button = render(<Button>{buttonLabel}</Button>);
 
-  expect(button.getByText(buttonLabel)).toBeInTheDocument();
+    expect(button.getByText(buttonLabel)).toBeInTheDocument();
+  });
+});
+
+describe('variant prop', () => {
+  test('Renders as primary button if a variant is not specified', () => {
+    const button = render(<Button>{buttonLabel}</Button>);
+
+    expect(button.container.firstChild).toHaveClass('btn-primary');
+  });
+
+  test('Render correct variants', () => {
+    const primaryButton = render(
+      <Button variant='primary'>{buttonLabel}</Button>
+    );
+    const secondaryButton = render(
+      <Button variant='secondary'>{buttonLabel}</Button>
+    );
+
+    expect(primaryButton.container.firstChild).toHaveClass('btn-primary');
+    expect(secondaryButton.container.firstChild).toHaveClass('btn-secondary');
+  });
 });
 
 const buttonLabel = 'Test';
