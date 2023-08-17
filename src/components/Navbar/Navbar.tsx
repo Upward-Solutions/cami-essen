a7fdb1844b9f3a9e094aa79d5324d8e3bf871b3e
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-17 13:25:47 -0300

    Added basic responsive padding on Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
   return (
     <header className='absolute top-0 z-50 flex items-center justify-center w-full h-12 bg-headlines'>
-      <div className='flex justify-between w-full desktop:max-w-3xl'>
+      <div className='flex justify-between w-full p-4 tablet:p-0 tablet:max-w-3xl'>
         <span>Cami Essen placeholder</span>
         <ul className='flex gap-4'>
           <li>FB</li>
