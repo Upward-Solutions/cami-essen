4e70a420b0157aec79eb436ade2ae4d8b3463dc0
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-16 22:34:25 -0300

    Added background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function HomePage() {
-  return <main className=''></main>;
+  return (
+    <main className=''>
+      <img
+        src='/background.webp'
+        alt='Cocina con distintos utencilios y sartén essen'
+      />
+    </main>
+  );
 }
