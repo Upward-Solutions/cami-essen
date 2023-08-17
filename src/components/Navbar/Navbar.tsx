56c691ac49def441e4e525dc89ddf3419f7fa330
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-17 13:10:09 -0300

    Added socials placeholder to Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <header className='absolute z-50 top-0 bg-headlines w-full'>
-      <span>Cami Essen placeholder</span>
+    <header className='absolute top-0 z-50 flex items-center justify-center w-full h-12 bg-headlines'>
+      <div className='flex justify-between w-full desktop:max-w-3xl'>
+        <span>Cami Essen placeholder</span>
+        <ul className='flex gap-4'>
+          <li>FB</li>
+          <li>TW</li>
+          <li>IG</li>
+        </ul>
+      </div>
     </header>
   );
 };
