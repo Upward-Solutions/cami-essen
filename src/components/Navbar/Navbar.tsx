4d048588b1b7d081157e2c81867e1c9723602db8
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-17 12:58:03 -0300

    Fixed Navbar z-index

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <header className='absolute z-10 top-0 bg-headlines w-full'>
+    <header className='absolute z-50 top-0 bg-headlines w-full'>
       <span>Cami Essen placeholder</span>
     </header>
   );
