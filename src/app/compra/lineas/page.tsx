4df0245d18858132100ba42cb69d953470565eac
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-24 15:34:51 -0300

    Added base ProductLinesPage

---
+++ src/app/compra/lineas/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ProductLinesPage() {
+  return (
+    <main className='relative h-[1280px] overflow-hidden bg-primary-hover'>
+      Page
+    </main>
+  );
+}
