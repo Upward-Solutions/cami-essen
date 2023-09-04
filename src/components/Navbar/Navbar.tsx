19362159743ea395757afe557df680010bea26ec
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-09-04 15:44:26 -0300

    Fixed Navbar styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <header className='absolute top-0 z-50 flex items-center justify-center w-full h-12 bg-headlines'>
+    <header className='absolute top-0 z-50 flex items-center justify-center w-full h-12 bg-card-background shadow-lg'>
       <div className='flex justify-between w-full p-4 tablet:p-0 tablet:max-w-3xl'>
         <span>Cami Essen placeholder</span>
         <ul className='flex gap-4'>
