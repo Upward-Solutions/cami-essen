2c3171d9ab2eef349d05d2deca1ad66db28c237f
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-09-04 16:11:48 -0300

    Added actions Buttons on ProductsPage

--- src/app/compra/lineas/[path]/productos/page.tsx
+++ src/app/compra/lineas/[path]/productos/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { getProductsByPath } from '@/services/backOffice';
+import { getProductLines, getProductsByPath } from '@/services/backOffice';
 import Card from '@/components/Card/Card';
 import ProductData from '@/components/ProductData/ProductData';
+import Button from '@/components/Button/Button';
 
 interface Props {
   params: {
@@ -11,6 +12,10 @@ interface Props {
 
 export default async function ProductsPage({ params }: Props) {
   const products = await getProductsByPath(params.path);
+  const productLines = await getProductLines();
+  const productLine = productLines.find(
+    (productLine) => productLine.path === params.path
+  );
 
   const renderProductCards = () => {
     return products.map((product) => (
@@ -23,9 +28,15 @@ export default async function ProductsPage({ params }: Props) {
   return (
     <main className='relative h-fit min-h-screen overflow-hidden bg-background'>
       <div className='flex justify-center w-full h-full my-16'>
-        <div className='flex flex-col tablet:max-w-3xl w-full'>
-          <h1>{params.path} Page</h1>
+        <div className='flex flex-col tablet:max-w-3xl w-full gap-4'>
+          <h1 className='text-headlines font-semibold text-xl'>
+            {productLine!.title} Page
+          </h1>
           <div className='grid grid-cols-2 gap-4'>{renderProductCards()}</div>
+          <div className='flex justify-between mt-4'>
+            <Button url='/compra/lineas'>Volver</Button>
+            <Button>Siguiente</Button>
+          </div>
         </div>
       </div>
     </main>
