8d257107caa6aa50d10d25045d7a2561e0d34565
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-09-04 12:42:32 -0300

    Added renderProductCards method on ProductsPage

--- src/app/compra/lineas/[path]/productos/page.tsx
+++ src/app/compra/lineas/[path]/productos/page.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { getProductsByPath } from '@/services/backOffice';
+import Card from '@/components/Card/Card';
+import ProductData from '@/components/ProductData/ProductData';
 
-export default function ProductsPage() {
+interface Props {
+  params: {
+    path: string;
+  };
+}
+
+export default async function ProductsPage({ params }: Props) {
+  const products = await getProductsByPath(params.path);
+
+  const renderProductCards = () => {
+    return products.map((product) => (
+      <Card key={`key-${product.title}`}>
+        <ProductData product={product} />
+      </Card>
+    ));
+  };
+
   return (
-    <main className='relative h-[1280px] overflow-hidden bg-primary'>Page</main>
+    <main className='relative h-fit min-h-screen overflow-hidden bg-background'>
+      <div className='flex justify-center w-full h-full my-16'>
+        <div className='flex flex-col tablet:max-w-3xl w-full'>
+          <h1>{params.path} Page</h1>
+          <div className='grid grid-cols-2 gap-4'>{renderProductCards()}</div>
+        </div>
+      </div>
+    </main>
   );
 }
